69d608c3a4b975d0be5c7e2fb778ea09d3c5f5dc
Falco Girgis <gyrovorbis@gmail.com>
2026-06-03 16:40:32 -0500

    Optional SH4ZAM acceleration + a few new features.
    1) CMakeLists.txt
        - Added support for new option, SDL_SH4ZAM, which adds -lsh4zam to
          linker libs + defines SDL_SH4ZAM for all .c files.
    2) dreamcast.sh
        - Added new option flag --enable-sh4zam, for enabling SH4ZAM
          acceleration within SDL3. Defaults to OFF.
    3) SDL_stdinc.h, SDL_memcpy.c, SDL_memmove.c
        - Mapped SDL_memcpy() and SDL_memmove() to shz_memcpy() and
          shz_memmove().
    4) SDL.c
        - Automatically register soft-reset button combination callback to
          call exit(0) from any SDL app, so user doesn't have to do it, and
          all examples not built specifically for DC now can exit to
          bootloader.
    5) SDL_random.c
         - Backed SDL_randf() with shz_randf().
    6) SDL_stdlib.c
         - Mapped almost every single single-precision floating-point SDL
           equivalent function to SH4ZAM's corresponding functions.
    7) SDL_systimer.c
         - Mapped SDL_PerformanceCounter API to actual SH4 perf counter API
           within KOS.

--- src/SDL.c
+++ src/SDL.c
@@ -60,6 +60,9 @@
 #ifdef SDL_PLATFORM_ANDROID
 #include "core/android/SDL_android.h"
 #endif
+#ifdef SDL_PLATFORM_DREAMCAST
+#include <kos.h>
+#endif
 
 #define SDL_ALL_SUBSYSTEM_FLAGS ~0U
 
@@ -281,6 +284,13 @@ bool SDL_IsVideoThread(void)
     return (SDL_GetCurrentThreadID() == SDL_VideoThreadID);
 }
 
+#ifdef SDL_PLATFORM_DREAMCAST
+static void DREAMCAST_resetCallback(uint8_t addr, uint32_t btns) {
+    printf("Software reset button sequence detected!\n");
+    exit(0);
+}
+#endif
+
 // Initialize all the subsystems that require initialization before threads start
 void SDL_InitMainThread(void)
 {
@@ -290,6 +300,10 @@ void SDL_InitMainThread(void)
     if (SDL_MainThreadID == 0) {
         SDL_MainThreadID = SDL_GetCurrentThreadID();
     }
+
+#ifdef SDL_PLATFORM_DREAMCAST
+    cont_btn_callback(0, CONT_RESET_BUTTONS, DREAMCAST_resetCallback);
+#endif
 
     SDL_InitTLSData();
     SDL_InitEnvironment();
--- src/stdlib/SDL_memmove.c
+++ src/stdlib/SDL_memmove.c
@@ -29,7 +29,9 @@
 #endif
 void *SDL_memmove(SDL_OUT_BYTECAP(len) void *dst, SDL_IN_BYTECAP(len) const void *src, size_t len)
 {
-#if defined(__GNUC__) && (defined(HAVE_LIBC) && HAVE_LIBC)
+#ifdef SDL_SH4ZAM
+    return shz_memmove(dst, src, len);
+#elif defined(__GNUC__) && (defined(HAVE_LIBC) && HAVE_LIBC)
     // Presumably this is well tuned for speed.
     return __builtin_memmove(dst, src, len);
 #elif defined(HAVE_MEMMOVE)
